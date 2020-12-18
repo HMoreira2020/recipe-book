b74d4e5868c036e9d196d96c711052f46899f247
HMoreira2020 <moreiraheather@gmail.com>
2020-12-17 20:36:21 -0700

    Create Recipe resource

---
+++ recipe-book-backend/db/migrate/20201218033604_create_recipes.rb
@@ -0,0 +1,15 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.sstring :image_url
+      t.string :overview
+      t.string :ingredients
+      t.string :cook_time
+      t.string :prep_time
+      t.text :instructions
+
+      t.timestamps
+    end
+  end
+end
