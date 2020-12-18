21507b0a7b39c230c031aecc57bf33a5a49c60a4
HMoreira2020 <moreiraheather@gmail.com>
2020-12-17 19:51:43 -0700

    first commit

---
+++ recipe-book-frontend/src/App.js
@@ -0,0 +1,18 @@
+// import { logo } from './logo'
+import React from 'react'
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+       
+      </header>
+    </div>
+  );
+}
+}
+
+export default App;
