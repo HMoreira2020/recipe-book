6b62a4214730b776d32b8bf6921d1828c42714b8
HMoreira2020 <moreiraheather@gmail.com>
2020-12-20 16:36:54 -0700

    Begin building books and recipes controllers

--- recipe-book-backend/app/controllers/books_controller.rb
+++ recipe-book-backend/app/controllers/books_controller.rb
@@ -1,2 +1,44 @@
 class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :update, :destroy]
+    # get '/books', to: 'book#index'
+   def index
+       books = Book.all 
+       render json: BookSerializer.new(books).to_serialized_json
+   end 
+
+   def create 
+       book = Book.new(book_params)
+       if book.save  
+           render json: BookSerializer.new(book).to_serialized_json, status: :created 
+       else
+           render json: { errors: book.errors.full_messages }, status: :not_acceptable
+       end
+
+   end 
+
+   def show 
+       render json: BookSerializer.new(books).to_serialized_json
+   
+   end 
+
+   def update
+       if book.update(book_params) 
+           render json: BookSerializer.new(book).to_serialized_json, status: :updated
+       else
+           render json: { errors: book.errors.full_messages }, status: :not_acceptable
+       end
+   end 
+
+   def destroy
+       book.destroy 
+   end 
+
+   private 
+   def set_book
+       @book = Book.find(params[:id])
+   end
+   
+   def book_params 
+       params.require(:book).permit(:title, :description)
+   end 
 end
--- recipe-book-backend/app/controllers/recipes_controller.rb
+++ recipe-book-backend/app/controllers/recipes_controller.rb
@@ -1,2 +1,44 @@
 class RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :update, :destroy]
+     # get '/recipes', to: 'recipe#index'
+    def index
+        recipes = Recipe.all 
+        render json: RecipeSerializer.new(recipes).to_serialized_json
+    end 
+
+    def create 
+        recipe = Recipe.new(recipe_params)
+        if recipe.save  
+            render json: RecipeSerializer.new(recipe).to_serialized_json, status: :created 
+        else
+            render json: { errors: recipe.errors.full_messages }, status: :not_acceptable
+        end
+
+    end 
+
+    def show 
+        render json: RecipeSerializer.new(recipes).to_serialized_json
+    
+    end 
+
+    def update
+        if recipe.update(recipe_params) 
+            render json: RecipeSerializer.new(recipe).to_serialized_json, status: :updated
+        else
+            render json: { errors: recipe.errors.full_messages }, status: :not_acceptable
+        end
+    end 
+
+    def destroy
+        recipe.destroy 
+    end 
+
+    private 
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+    
+    def recipe_params 
+        params.require(:recipe).permit(:name, :image_url, :overview, :ingredients, :cook_time, :prep_time, :instructions, :book_id)
+    end 
 end
