4ee3d7dfbd84c6dc511bba807a65dab566606abb
HMoreira2020 <moreiraheather@gmail.com>
2020-12-20 21:01:55 -0700

    Install redux, react-thunk, react-redux, react-router-dom and import files dependencies in index.js

--- recipe-book-frontend/src/App.js
+++ recipe-book-frontend/src/App.js
@@ -3,12 +3,19 @@ import React from 'react'
 import './App.css';
 
 class App extends React.Component {
+
+  componentDidMount(){
+    fetch("http://localhost:3000/recipes")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+  
   render() {
     return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-       
+       App 
       </header>
     </div>
   );
--- recipe-book-frontend/src/index.js
+++ recipe-book-frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'react-thunk'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <App />,
