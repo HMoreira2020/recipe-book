ba0ea4b093c8893f93c1070c128c36bb0b87ffd7
HMoreira2020 <moreiraheather@gmail.com>
2020-12-22 21:04:29 -0700

    Fix type in addRecipe.js

--- recipe-book-frontend/src/actions/addRecipe.js
+++ recipe-book-frontend/src/actions/addRecipe.js
@@ -10,7 +10,7 @@ export const addRecipe = (recipe) => {
             body: JSON.stringify(recipe)
         })
         .then(response => response.json())
-        .then(book => dispatch({type: "ADD_RECIPE", payload: recipe}))
+        .then(recipe => dispatch({type: "ADD_RECIPE", payload: recipe}))
         //takes json response and dispatches it to reducer to update state with new book
         //right away adding to redux store without refetching the data here 
     };
