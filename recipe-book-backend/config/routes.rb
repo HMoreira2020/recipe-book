190f99b2ad500ae4cd6a16669c013126aa197324
HMoreira2020 <moreiraheather@gmail.com>
2021-01-13 12:46:15 -0700

    Update config routes folder

--- recipe-book-backend/config/routes.rb
+++ recipe-book-backend/config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   # resources :books do 
   #   resources :recipes [:create, :update, :destroy]
   # end
-  
   resources :recipes, only: [:index]  
+
   resources :books, only: [:index, :create, :update, :destroy] do 
     resources :recipes [:create, :update, :destroy]
   end
+
 end
 
 
@@ -15,7 +16,7 @@ end
 # "http://localhost:3000/books/${bookId}/recipes" create recipe nested
 # "http://localhost:3000/books/${bookId}" destroy book
 # "http://localhost:3000/books/${bookId}/recipes/${recipeId}" destroy recipe nested 
-# "`http://localhost:3000/books/${bookId}"  update book 
+# "http://localhost:3000/books/${bookId}"  update book 
 # "http://localhost:3000/books/${bookId}/recipes/${recipeId}" update recipe nested 
 # "http://localhost:3000/books" books index 
 # "http://localhost:3000/recipes" recipes index
